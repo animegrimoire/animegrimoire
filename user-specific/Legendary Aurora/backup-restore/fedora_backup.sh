1f74c27495c95ac0845dd4e47d119402f6bc9cfa
initramfs <gksu@tutanota.de>
2019-11-28 15:03:11 +0000

    Simple backup-restore script for Fedora host.

---
+++ user-specific/Legendary Aurora/backup-restore/fedora_backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+clear
+echo 'Select action'
+echo '1) Backup'
+echo '2) Restore'
+echo 'q) Exit'
+read -p '<1/2/q>: ' action
+
+case $action in
+	[1] )
+	tar -cvf backup.tar ~/.local/bin   
+	tar -uvf backup.tar ~/.local/preset
+	tar -uvf backup.tar ~/.config/deluge
+	tar -uvf backup.tar ~/.config/rclone
+	tar -uvf backup.tar ~/.ssh/id_rsa
+	tar -uvf backup.tar ~/.ssh/id_rsa.pub
+	sudo tar -uvf backup.tar /etc/openvpn/client
+	;;
+
+	[2] )
+	if [ -f "~/backup.tar" ]; then
+		echo 'backup found, restoring.. ' 
+		sudo tar -xvf ~/backup.tar
+	else 
+		echo "backup does not exist. input your 'backup.tar' in absolute path:"
+		read -p 'archive location: ' custom_archive
+		sudo tar -xvf $custom_archive
+	fi
+	;;
+
+	[q] )
+	exit
+	;;
+esac
