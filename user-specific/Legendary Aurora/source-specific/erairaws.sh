a75b0115f5fa1ae5043723634a57141b036016c8
initramfs <gksu@tutanota.de>
2019-12-12 05:30:49 +0000

    "$2" input should works properly now.

--- user-specific/Legendary Aurora/source-specific/erairaws.sh
+++ user-specific/Legendary Aurora/source-specific/erairaws.sh
@@ -11,7 +11,6 @@ startl=$(date +%s)
 #
 #    USAGE: ./erairaws-multisub.sh '[Erai-raws] Choujin - 01 [720p][Multiple Subtitle].mkv' 47
 
-
 #	Logging functions
 readonly l="animegrimoire_enc$(date +%d%m%H%M).log"
 exec 3>&1 4>&2
@@ -38,15 +37,12 @@ subtitle="$(echo "$1" | cut -f 1 -d '.').ass"
 fansub="$(echo $1 | cut -d "[" -f2 | cut -d "]" -f1)"
 preset="/home/$USER/.local/preset/x264_Animegrimoire.json"
 
-# Remove unnecessary attachment, keep only Video, Audio, English Subtitle
-ffmpeg -i "$1" -map 0:0 -map 0:1 -map 0:2 -c:v copy -c:a copy -c:s:2 copy -metadata:s:a:0 language=jpn "$1_trim.mkv" -y
-
 # Extract fonts, install and update cache
-ffmpeg -dump_attachment:t "" -i "$1_trim.mkv" -y
+ffmpeg -dump_attachment:t "" -i "$1" -y
 for fonts in *.*TF *.*tf; do rclone -vv copy $fonts /home/$USER/.fonts; done
 fc-cache -f -v
 
-# Remove CRC32 value from input files
+# Remove value from input files
 if [[ -z "$2" ]]; then
     :
 else
@@ -56,31 +52,39 @@ else
   rm -rfv namehold
 fi
 
-# Stage 1:	Extract Subtitle from $1(mkv)
-/usr/bin/ffmpeg -i "$1_trim.mkv" -map 0:s "$subtitle" -y
+# Stage 0:  Remove unnecessary attachment, keep only Video, jpn Audio, eng Subtitle
+ffmpeg -i "$input" -map 0:0 -map 0:1 -map 0:2 -c:v copy -c:a copy -c:s:2 copy -metadata:s:a:0 language=jpn "$1_trim.mkv" -y
 
-# Stage 2:	demux $1(mkv), remove original subtitle
-/usr/bin/ffmpeg -i "$1_trim.mkv" -map 0 -map 0:s -codec copy "$1_tmp.mkv" -y
+# Stage 1:  Register the newly created mkv as input and register new output file name
+mv -v "$input" "$input_tmp.mkv"
+mv -v "$1_trim.mkv" "$input"
+output="$(echo "$input" | cut -f 1 -d '.').mp4"
 
-# Stage 3:	embed watermark to comply animegrimoire's global rule
+# Stage 2:	Extract Subtitle from $1(mkv)
+/usr/bin/ffmpeg -i "$input" -map 0:s "$subtitle" -y
+
+# Stage 3:	demux $1(mkv), remove original subtitle
+/usr/bin/ffmpeg -i "$input" -map 0 -map 0:s -codec copy "$1_tmp.mkv" -y
+
+# Stage 4:	embed watermark to comply animegrimoire's global rule
 sed '/Format\: Name/a Style\: Watermark,Cambria,12,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,1,0,0,100,100,0,0,1,2,1.2,9,10,10,10,1' "$subtitle" > "modified_sub.tmp1"
 sed '/Format\: Layer/a Dialogue\: 0,0:00:00.00,0:00:02.00,Watermark,,0000,0000,0000,,animegrimoire.org' "modified_sub.tmp1" > "$subtitle"
 
-# Stage 4:	send back the modified subtitle into $1(mkv) container
+# Stage 5:	send back the modified subtitle into $1(mkv) container
 /usr/bin/ffmpeg -i "$1_tmp.mkv" -i "$subtitle" -c:v copy -c:a copy -c:s copy -map 0:0 -map 0:1 -map 1:0 -metadata:s:s:0 language=eng "$1_sub.mkv" -y
 
-# Stage 5:	send subbed $1 to HandBrakeCLI encoder with animegrimoire's preset.
+# Stage 6:	send subbed $1 to HandBrakeCLI encoder with animegrimoire's preset.
 #			Make sure you have compiled fdk_aac version as stated in guide thread.
 /usr/local/bin/HandBrakeCLI --preset-import-file $preset -Z "x264_Animegrimoire" -i "$1_sub.mkv" -o "$output"
 
-# Stage 6:	Rename file names and embed CRC32 in end of encoded file (case sensitive).
+# Stage 7:	Rename file names and embed CRC32 in end of encoded file (case sensitive).
 /usr/bin/rename -v $fansub animegrimoire "$output" > hold.name
 /usr/bin/rhash --embed-crc --embed-crc-delimiter='' "$(cat hold.name | cut -d "\`" -f3 | cut -d "'" -f 1)" && rm -v hold.name
 
 # Clean up. 
 rm -v *.*tf *.*TF
 rm -v *.tmp* ; rm -v *.ass ; rm -v "$1_sub.mkv" ; rm -v "$1_tmp.mkv"
-mv -v "$1" ../finish_encoded
+mv -v "$input" ../finish_encoded
 
 # Move output files to syncthing folder
 for files in \[animegrimoire\]\ *.mp4; do mv -v "$files" ../finish_uploaded/; done
